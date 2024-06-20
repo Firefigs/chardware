903ea6e48bb02fcb0bd1b82bd5a6a0b222dbe6f8
Firefigs <153876013+Firefigs@users.noreply.github.com>
2024-06-20 03:04:56 +0000

    final

--- scripts.js
+++ scripts.js
@@ -1,11 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('JavaScript is working!');
 
-    // Toggle description visibility on click
     const sections = document.querySelectorAll('section');
     for (const section of sections) {
         section.addEventListener('click', function() {
-            // Toggle 'clicked' class
             this.classList.toggle('clicked');
         });
     }
